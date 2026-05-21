c4ff374632262e8b36cc435a18ccc9d6ea06d842
Tabakov Ivan <ivan.tabakov.10@gmail.com>
2026-05-21 14:26:40 +0500

    Add files via upload

--- app/src/main/java/com/example/practica3/MainActivity.java
+++ app/src/main/java/com/example/practica3/MainActivity.java
@@ -19,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
 
     private ConstraintLayout myConstraintLayout;
     private TextView myTextView;
+    private Button redButton;
+    private Button greenButton;
+    private Button yellowButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +30,22 @@ public class MainActivity extends AppCompatActivity {
 
         myConstraintLayout = findViewById(R.id.main);
         myTextView = findViewById(R.id.textView);
+        redButton = findViewById(R.id.button_red);
+        greenButton = findViewById(R.id.button_green);
+        yellowButton = findViewById(R.id.button_yellow);
 
-        Button yellowButton = findViewById(R.id.button_yellow);
         yellowButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
             { myTextView.setText(R.string.yellow);
-                myConstraintLayout.setBackgroundColor(ContextCompat
+                //myConstraintLayout.setBackgroundColor(ContextCompat
+                        //.getColor(MainActivity.this, R.color.yellowColor));
+                yellowButton.setBackgroundColor(ContextCompat
                         .getColor(MainActivity.this, R.color.yellowColor));
+                greenButton.setBackgroundColor(ContextCompat
+                        .getColor(MainActivity.this, R.color.purple));
+                redButton.setBackgroundColor(ContextCompat
+                        .getColor(MainActivity.this, R.color.purple));
             }
         });
 
@@ -43,12 +54,18 @@ public class MainActivity extends AppCompatActivity {
     public void onGreenButtonClick(View view) {
 
         myTextView.setText(R.string.green);
-        myConstraintLayout.setBackgroundColor(ContextCompat.getColor(this,R.color.greenColor));
+        //myConstraintLayout.setBackgroundColor(ContextCompat.getColor(this,R.color.greenColor));
+        greenButton.setBackgroundColor(ContextCompat.getColor(this, R.color.greenColor));
+        redButton.setBackgroundColor(ContextCompat.getColor(this, R.color.purple));
+        yellowButton.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.purple));
     }
 
     public void onRedButtonClick(View view) {
         myTextView.setText(R.string.red);
-        myConstraintLayout.setBackgroundColor(ContextCompat.getColor(this,R.color.redColor));
+        //myConstraintLayout.setBackgroundColor(ContextCompat.getColor(this,R.color.redColor));
+        redButton.setBackgroundColor(ContextCompat.getColor(this, R.color.redColor));
+        greenButton.setBackgroundColor(ContextCompat.getColor(this, R.color.purple));
+        yellowButton.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.purple));
     }
 
 
